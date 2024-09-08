78742bc0d5de42b24c0a3c5d2c514ded152afd88
Aaron Perez <aap7640@gmail.com>
2024-09-08 15:54:55 -0500

    Fixed options in the virsh invocation to properly remove VM, and removed VM entry from known_hosts file

--- remove_medal.sh
+++ remove_medal.sh
@@ -5,6 +5,7 @@ export PATH="${SELF%/*}:$PATH"
 
 PROGRAM="${0##*/}"
 ARGS=( "$@" )
+HOME_DIR="/home/$(logname)"
 
 auto_su() {
   [[ $UID == 0 ]] || exec sudo -p "$PROGRAM must be run as root. Please enter the password for %u to continue: " -- "$BASH" -- "$SELF" "${ARGS[@]}"
@@ -13,6 +14,10 @@ auto_su() {
 num=$1
 
 auto_su
-virsh --connect qemu:///system undefine --domain medal-test${num}
-rm -r /var/lib/libvirt/images/medal/test${num}
+virsh --connect qemu:///system shutdown --domain medal-test${num}
+sleep 5s
+virsh --connect qemu:///system undefine --domain medal-test${num} --snapshots-metadata --delete-storage-volume-snapshots
+rm -r /var/lib/libvirt/images/medal/test${num} 
+ssh-keygen -f "$HOME_DIR/.ssh/known_hosts" -R "medal-test${num}.medal.lan"
+
 
